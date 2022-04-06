0fb04221411e0bbba984e8f2b02fd16c6758b770
“Ahad <ahussain@dev-10.com>
2022-04-06 12:15:37 -0400

    Added Validate Guess Function

--- Word-Game.py
+++ Word-Game.py
@@ -1,5 +1,5 @@
-correct_word = "champion"
-number_of_guesses = 7
+from PyDictionary import PyDictionary
+dictionary = PyDictionary()
 
 def Guess_Choice():
     guess_choice = input('Do you want to guess a letter or a word?: ')
@@ -14,7 +14,21 @@ def Guess_Choice():
             guess_choice = 1
         else:
             guess_choice = input('Invalid input. Do you want to guess a letter or a word?: ')
+    return guess_choice
 
-# def Validate_Guess(guess_choice):
-#     if guess_choice = 0:
+def Validate_Guess(choice):
+    if choice == 0:
+        guess = input("Guess a word: ")
+        valid_word = bool(dictionary.meaning(guess, True))
+        while valid_word == False:
+            guess = input("Invalid input. Guess a word: ")
+            valid_word = bool(dictionary.meaning(guess,True))
+    else:
+        guess = input("Guess a letter: ")
+        valid_letter = guess.isalpha()
+        while valid_letter == False or len(guess) != 1:
+            guess = input("Invalid input. Guess a letter: ")
+            valid_letter = guess.isalpha()
+    return guess.lower()
 
+print(Validate_Guess(Guess_Choice()))
