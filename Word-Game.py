16655fc30c51eb6d53cdead8cbe5913af0205131
“Ahad <ahussain@dev-10.com>
2022-04-06 13:54:42 -0400

    ValidateGuess() tracks current status of game

--- Word-Game.py
+++ Word-Game.py
@@ -1,8 +1,17 @@
 from PyDictionary import PyDictionary
 dictionary = PyDictionary()
 
+correct_word = "champion".lower()
+list_of_letters = []
+
+for letter in correct_word:
+    list_of_letters.append("_")
+
+print(' '.join(list_of_letters), end = "\n\n" )
+
 def Guess_Choice():
     guess_choice = input('Do you want to guess a letter or a word?: ')
+    print()
     valid_choice = False
 
     while (valid_choice == False):
@@ -21,14 +30,28 @@ def Validate_Guess(choice):
         guess = input("Guess a word: ")
         valid_word = bool(dictionary.meaning(guess, True))
         while valid_word == False:
-            guess = input("Invalid input. Guess a word: ")
+            guess = input("Invalid input. Guess a word: ").lower()
             valid_word = bool(dictionary.meaning(guess,True))
+        if guess == correct_word:
+            print("You win!")
+        else:
+            print("Incorrect! You lose a guess!")
     else:
         guess = input("Guess a letter: ")
+        print()
         valid_letter = guess.isalpha()
         while valid_letter == False or len(guess) != 1:
-            guess = input("Invalid input. Guess a letter: ")
+            guess = input("Invalid input. Guess a letter: \n\n").lower()
             valid_letter = guess.isalpha()
-    return guess.lower()
+        if any(letter in correct_word for letter in guess):
+            print("Correct!")
+            for i in range(0,len(correct_word)):
+                if guess == list(correct_word)[i]:
+                    list_of_letters[i] = guess
+        else:
+            print("Incorrect! You lose a guess!\n\n")
 
-print(Validate_Guess(Guess_Choice()))
+        print(' '.join(list_of_letters))
+            
+
+Validate_Guess(Guess_Choice())
